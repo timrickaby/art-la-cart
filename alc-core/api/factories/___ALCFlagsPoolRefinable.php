786786af030d4148bb2029cf2dbdd58d3256f64b
Tim Rickaby <hello@timrickaby.com>
2022-04-30 16:55:26 +0100

    Updates to file header comments.

--- alc-core/api/factories/___ALCFlagsPoolRefinable.php
+++ alc-core/api/factories/___ALCFlagsPoolRefinable.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * 
- * Name:     		Art La Cart
- * Product URI:		https://artlacart.com
- * Description:		Content Management System with Events, Galleries and Basket Support
- * Version:			1.0.0
- * Author:			Tim Rickaby
- * Author URI:		https://timrickaby.com & https://modocodo.com
- * Copyright:		© 2011 Tim Rickaby
+ * Name: Art La Cart
+ * Product URI: https://artlacart.com
+ * Description: Content Management System with Events, Galleries and Basket Support
+ * Version: 1.0.0
+ * Author: Tim Rickaby
+ * Author URI: https://timrickaby.com & https://modocodo.com
+ * Copyright: © 2011 Tim Rickaby
  * 
  */
 
